9fac0aaa20b9091e71beb31e55eead76ce8ad197
Patrick Hunt <phunt1@gmail.com>
2009-10-05 16:57:47 -0700

    added acl support on znodes

--- zktree/models.py
+++ zktree/models.py
@@ -4,6 +4,13 @@ from datetime import datetime
 import threading
 import zookeeper
 
+PERM_READ = 1
+PERM_WRITE = 2
+PERM_CREATE = 4
+PERM_DELETE = 8
+PERM_ADMIN = 16
+PERM_ALL = PERM_READ | PERM_WRITE | PERM_CREATE | PERM_DELETE | PERM_ADMIN
+
 zookeeper.set_log_stream(open("cli_log.txt","w"))
 
 TIMEOUT = 10.0
@@ -41,6 +48,9 @@ class ZKClient(object):
     def get_children(self, path, watcher=None):
         return zookeeper.get_children(self.handle, path, watcher)
 
+    def get_acls(self, path):
+        return zookeeper.get_acl(self.handle, path)
+
 ZOOKEEPER_SERVERS = getattr(settings,'ZOOKEEPER_SERVERS')
 
 class ZNode(object):
@@ -52,5 +62,22 @@ class ZNode(object):
             self.stat['ctime'] = datetime.fromtimestamp(self.stat['ctime']/1000)
             self.stat['mtime'] = datetime.fromtimestamp(self.stat['mtime']/1000)
             self.children = zk.get_children(path) or []
+            self.acls = zk.get_acls(path)[1] or []
+            for acl in self.acls:
+                perms = acl['perms']
+                perms_list = []
+                if perms & PERM_READ:
+                    perms_list.append("PERM_READ")
+                if perms & PERM_WRITE:
+                    perms_list.append("PERM_WRITE")
+                if perms & PERM_CREATE:
+                    perms_list.append("PERM_CREATE")
+                if perms & PERM_DELETE:
+                    perms_list.append("PERM_DELETE")
+                if perms & PERM_ADMIN:
+                    perms_list.append("PERM_ADMIN")
+                if perms & PERM_ALL == PERM_ALL:
+                    perms_list = ["PERM_ALL"]
+                acl['perm_list'] = perms_list
         finally:
             zk.close()
